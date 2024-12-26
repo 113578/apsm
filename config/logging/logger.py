15d86797ebe83bd39cd80fdbbe1ff7624d95babe
113478. <81105698+113578@users.noreply.github.com>
2024-12-26 18:36:50 +0300

    Update logger.py

--- config/logging/logger.py
+++ config/logging/logger.py
@@ -2,8 +2,6 @@ import os
 import logging
 import logging.config
 
-from logging.handlers import RotatingFileHandler
-
 
 logging.config.fileConfig(os.getenv('PYTHONPATH') + '/config/logging/config')
 
